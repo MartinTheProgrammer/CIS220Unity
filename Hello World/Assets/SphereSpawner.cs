2cb2fa556b840876ee9ca17f0a97dca999baf6d8
Martin Belt <martinbarry2020@gmail.com>
2019-02-01 08:23:06 -0600

    Hello World commit

---
+++ Hello World/Assets/SphereSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereSpawner : MonoBehaviour
+{
+    public GameObject spherePrefabVar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(spherePrefabVar);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(spherePrefabVar);   
+    }
+}
